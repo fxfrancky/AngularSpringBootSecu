0849153abba681c81bdfd330dfa44fc3e6a30f06
fxjon <fxjon@DESKTOP-5RTCK5J>
2018-03-05 22:40:54 +0100

    first commit for SpringBoot 2

---
+++ src/main/java/com/angularsecure/AngularSpringBootSecuApplication.java
@@ -0,0 +1,12 @@
+package com.angularsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootSecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootSecuApplication.class, args);
+	}
+}
